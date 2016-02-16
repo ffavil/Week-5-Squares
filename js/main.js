30a4fc22e629340c8c986ca5d4a8e7e8c3117401
Graphic <graphic@SOD-SJH-605-05M.local>
2016-02-16 15:41:29 -0500

    week6

--- js/main.js
+++ js/main.js
@@ -1,18 +1,51 @@
+// var wimg = $('.galimg').width();
+// $('.galimg').height(wimg);
+
+// $('.galimg img').each(function(){
+
+// 	var w = $(this).width();
+// 	var h = $(this).height();
+
+// 	if(w > h){
+// 		$(this).addClass('landscape');
+// 		$('.landscape').css('text-align' ,'center');
+// 	}
+
+// 	if (h > w){
+// 		$(this).addClass('portrait');
+// 	}
+	
+// });
+
 var wimg = $('.galimg').width();
-$('.galimg').height(wimg);
+var himg = $('.galimg').height(wimg).height();
+console.log(wimg, himg);
 
 $('.galimg img').each(function(){
 
 	var w = $(this).width();
 	var h = $(this).height();
+	console.log(w, h);
 
-	if(w > h){
+	if(w > h) {
 		$(this).addClass('landscape');
-		$('.landscape').css('text-align' ,'center');
 	}
 
-	if (h > w){
+	else if (h > w) {
 		$(this).addClass('portrait');
 	}
-	
+
+	var nw = $(this).width();
+	var nh = $(this).height();
+	console.log(nw, nh);
+
+	if (w > h){
+		$(this).css('left', '-' + ((nw-wimg)/2) + 'px')
+	}
+	if (h > w){
+		$(this).css('bottom', ((nh-himg)/2) + 'px')
+
+	console.log((nh-himg)/2);
+	};
+
 });
